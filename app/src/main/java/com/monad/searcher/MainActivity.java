bd17ec773ea2dd8908f4242f7755990c46449296
DongJu\Dong Ju <ehdwn160@naver.com>
2017-06-30 17:51:27 +0900

    탭레이아웃 완료

--- app/src/main/java/com/monad/searcher/MainActivity.java
+++ app/src/main/java/com/monad/searcher/MainActivity.java
@@ -1,13 +1,48 @@
 package com.monad.searcher;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+
+    private ViewPager viewPager;
+    private FragmentAdapter adapter;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.viewpager, new MyFragment()).commit();
+        tabLayout();
+    }
+
+    private void tabLayout() {
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        adapter = new FragmentAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MyFragment(), "홈");
+        viewPager.setAdapter(adapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(this);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
     }
 }
---
+++ app/src/main/java/com/monad/searcher/MyFragment.java
@@ -0,0 +1,19 @@
+package com.monad.searcher;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyFragment extends Fragment {
+    private View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_1, container, false);
+        return v;
+    }
+}
