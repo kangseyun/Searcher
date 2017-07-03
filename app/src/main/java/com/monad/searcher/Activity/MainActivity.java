2d6fdd81379c627c72328734ca100740e6cde976
DongJu\Dong Ju <ehdwn160@naver.com>
2017-07-03 20:31:55 +0900

    Main

--- app/src/main/java/com/monad/searcher/Activity/MainActivity.java
+++ app/src/main/java/com/monad/searcher/Activity/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         title = (TextView) findViewById(R.id.toolbar_title);
         setting = (ImageView) findViewById(R.id.setting);
-        title.setText("홈");
+        title.setText("Searcher");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
