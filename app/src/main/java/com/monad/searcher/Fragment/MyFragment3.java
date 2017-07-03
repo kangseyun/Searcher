aba2414bdc4a0e2701adc9d3544b16d38dbf4713
DongJu\Dong Ju <ehdwn160@naver.com>
2017-07-03 21:19:39 +0900

    Fragment3 Layout update

--- app/src/main/java/com/monad/searcher/Fragment/MyFragment3.java
+++ app/src/main/java/com/monad/searcher/Fragment/MyFragment3.java
@@ -3,19 +3,51 @@ package com.monad.searcher.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.monad.searcher.Adapter.RecyclerViewAdapter;
+import com.monad.searcher.Model.MyData;
 import com.monad.searcher.R;
+
+import java.util.ArrayList;
 
 public class MyFragment3 extends Fragment {
     private View v;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<MyData> myDataset;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_3, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.my_recycler_view);
+        setRecyclerView();
         return v;
+    }
+
+    private void setRecyclerView()
+    {
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        myDataset = new ArrayList<>();
+        mAdapter = new RecyclerViewAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        myDataset.add(new MyData("현대증권", "+19.42%" , "+7.31%"));
+        myDataset.add(new MyData("삼성전자", "-13.78%" , "-7.36%"));
+        myDataset.add(new MyData("LG화학", "+3.21%" , "+7.49%"));
     }
 }
---
+++ app/src/main/java/com/monad/searcher/Model/MyData.java
@@ -0,0 +1,11 @@
+package com.monad.searcher.Model;
+
+public class MyData {
+    public String name,KOSPI,KOSDAQ;
+
+    public MyData(String name, String KOSPI, String KOSDAQ) {
+        this.name = name;
+        this.KOSPI = KOSPI;
+        this.KOSDAQ = KOSDAQ;
+    }
+}
