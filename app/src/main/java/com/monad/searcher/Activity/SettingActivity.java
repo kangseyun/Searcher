efc23d1acd995a00cfba369c51fcf26ba58da157
DongJu\Dong Ju <ehdwn160@naver.com>
2017-07-20 22:48:52 +0900

    google sign in 구현 완료

--- app/src/main/java/com/monad/searcher/Activity/SettingActivity.java
+++ app/src/main/java/com/monad/searcher/Activity/SettingActivity.java
@@ -1,11 +1,9 @@
 package com.monad.searcher.Activity;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -41,5 +39,4 @@ public class SettingActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(menuItem);
     }
-
 }
